43bf46c4fa225130156a6e3af8db7c8a40e1e1ff
Steffen <per.sc@di.ku.dk>
2021-04-16 10:13:02 +0200

    best working script

--- scripts/train_registration.sh
+++ scripts/train_registration.sh
@@ -11,6 +11,6 @@ else
     WRAPPER_FUNC=
 fi
 
-$WRAPPER_FUNC python -m src.train_registration --dataset brain2d --channels 64 128 256 --batch_size 32 --gpus -1 --accelerator dp --max_epochs 80 --lr_decline_patience 10 --early_stop_patience 25 --conv_layers_per_stage 1 --bnorm --dropout --notest
-$WRAPPER_FUNC python -m src.train_registration --dataset brain2d --channels 64 128 256 --batch_size 32 --gpus -1 --accelerator dp --max_epochs 80 --lr_decline_patience 10 --early_stop_patience 25 --conv_layers_per_stage 1 --bnorm --dropout --integrate --notest
-$WRAPPER_FUNC python -m src.train_registration --dataset brain2d --channels 64 128 256 --batch_size 32 --gpus -1 --accelerator dp --max_epochs 80 --lr_decline_patience 10 --early_stop_patience 25 --conv_layers_per_stage 1 --bnorm --dropout --per_pixel_prior --notest
+$WRAPPER_FUNC python -m src.train_registration --dataset brain2d --channels 64 128 256 --batch_size 32 --gpus -1 --accelerator dp --max_epochs 800 --lr_decline_patience 20 --early_stop_patience 150 --conv_layers_per_stage 1 --bnorm --dropout --notest
+#$WRAPPER_FUNC python -m src.train_registration --dataset brain2d --channels 64 128 256 --batch_size 32 --gpus -1 --accelerator dp --max_epochs 80 --lr_decline_patience 10 --early_stop_patience 25 --conv_layers_per_stage 1 --bnorm --dropout --integrate --notest
+#$WRAPPER_FUNC python -m src.train_registration --dataset brain2d --channels 64 128 256 --batch_size 32 --gpus -1 --accelerator dp --max_epochs 80 --lr_decline_patience 10 --early_stop_patience 25 --conv_layers_per_stage 1 --bnorm --dropout --per_pixel_prior --notest
