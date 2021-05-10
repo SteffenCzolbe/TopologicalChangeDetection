c4e40e7dc1ddec9d3a995903708077d18ed7b555
Steffen <per.sc@di.ku.dk>
2021-05-10 18:05:00 +0200

    updated

--- scripts/train_registration.sh
+++ scripts/train_registration.sh
@@ -13,10 +13,10 @@ else
 fi
 
 # trainable recon, analytical prior
-$WRAPPER_FUNC python3 -m src.train_registration --dataset brain2d --channels 64 128 256 --analytical_prior --trainable_recon --recon_weight_init -5 --gpus -1 --accelerator dp --max_epochs 300 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --dropout --notest --fast_dev_run $FAST_DEV_RUN
+$WRAPPER_FUNC python3 -m src.train_registration --dataset brain2d --channels 64 128 256 --analytical_prior --trainable_recon --recon_weight_init -5 --gpus -1 --accelerator dp --max_epochs 200 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --dropout --notest --fast_dev_run $FAST_DEV_RUN
 
 # semantic loss, trainable recon, analytical prior
-$WRAPPER_FUNC python3 -m src.train_registration --dataset brain2d --channels 64 128 256 --analytical_prior --trainable_recon --semantic_loss trained_models/semantic_loss --recon_weight_init -12.5 --gpus -1 --accelerator dp --max_epochs 300 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --dropout --notest --fast_dev_run $FAST_DEV_RUN
+$WRAPPER_FUNC python3 -m src.train_registration --dataset brain2d --channels 64 128 256 --analytical_prior --trainable_recon --semantic_loss trained_models/semantic_loss --recon_weight_init -12.5 --gpus -1 --accelerator dp --max_epochs 200 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --dropout --notest --fast_dev_run $FAST_DEV_RUN
 
 # fixed prior parameters alpha, beta
 $WRAPPER_FUNC python -m src.train_registration --dataset brain2d --channels 64 128 256 --prior_weights_init 0 0 --recon_weight_init -5 --trainable_recon --gpus -1 --accelerator dp --max_epochs 200 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --dropout --notest --fast_dev_run $FAST_DEV_RUN
