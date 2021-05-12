1d2102aed4ef486ec3b965d1705c2a64fd0debec
Steffen <per.sc@di.ku.dk>
2021-05-12 14:48:54 +0200

    cleaned up elbo, notation like paper

--- src/models/elbo.py
+++ src/models/elbo.py
@@ -7,28 +7,34 @@ import src.util as util
 
 
 class ELBO(nn.Module):
-    def __init__(self, data_dims, use_analytical_prior, use_analytical_recon, semantic_loss, init_prior_log_alpha, init_prior_log_beta, trainable_prior, init_recon_log_var, trainable_recon_var):
+    def __init__(self, data_dims, semantic_loss, init_recon_log_var):
         super().__init__()
         self.ndims = torchreg.settings.get_ndims()
         self.data_dims = data_dims
-        self.use_analytical_prior = use_analytical_prior
-        self.use_analytical_recon = use_analytical_recon
         self.semantic_loss = semantic_loss
         if self.semantic_loss:
             self.load_semantic_loss_model(model_path=semantic_loss)
         self.pi = torch.as_tensor(3.14159)
 
-        self.prior_log_alpha = torch.nn.parameter.Parameter(
-            torch.as_tensor(init_prior_log_alpha, dtype=torch.float32), requires_grad=trainable_prior)
-        self.prior_log_beta = torch.nn.parameter.Parameter(
-            torch.as_tensor(init_prior_log_beta, dtype=torch.float32), requires_grad=trainable_prior)
         if self.data_dims[0] > 1:
             init_recon_log_var = [init_recon_log_var] * self.data_dims[0]
         self.recon_log_var = torch.nn.parameter.Parameter(
-            torch.as_tensor(init_recon_log_var, dtype=torch.float32), requires_grad=trainable_recon_var)
+            torch.as_tensor(init_recon_log_var, dtype=torch.float32), requires_grad=True)
+        self.running_mean_log_alpha = nn.BatchNorm1d(
+            1, track_running_stats=True)
+        self.running_mean_log_beta = nn.BatchNorm1d(
+            1, track_running_stats=True)
         self.grad_norm = torchreg.metrics.GradNorm(
             penalty="l2", reduction="none")
         self.transformer = torchreg.nn.SpatialTransformer()
+
+    @property
+    def log_alpha(self):
+        return self.running_mean_log_alpha.running_mean.squeeze()
+
+    @property
+    def log_beta(self):
+        return self.running_mean_log_beta.running_mean.squeeze()
 
     def load_semantic_loss_model(self, model_path):
         # load semantic loss model
@@ -49,15 +55,9 @@ class ELBO(nn.Module):
 
     def recon_loss(self, I0, I1, transform, reduction='mean'):
         # we implement the term pixel-whise, and mean over pixels if specified by the reduction
-        # the scalar term is devided by factor p (canceled out), as it will be expanded (broadcasted) to size p during summation of the loss terms
-        p = torch.prod(torch.tensor(self.data_dims[1:]))  # p pixels
-        c = self.data_dims[0]  # c channels
-
-        def expect(t):
-            # sum spatial dimensions
-            t = t.sum(dim=[2, 3, 4], keepdim=True)
-            # mean (expectation) over the batch and channels
-            return t.mean(dim=[0, 1], keepdim=True)
+        # the scalar term is devided by factor n (canceled out), as it will be expanded (broadcasted) to size n during summation of the loss terms
+        n = torch.prod(torch.tensor(self.data_dims[1:]))  # n pixels
+        D = self.data_dims[0]  # D channels
 
         if self.semantic_loss:
             I0 = self.semantic_loss_model.augment_image(I0)
@@ -67,21 +67,12 @@ class ELBO(nn.Module):
 
         diff = (I1 - I01)**2
 
-        if self.use_analytical_recon:
-            var = 1 / (p * expect(diff))
-            log_var = torch.log(var)
-        else:
-            var = torch.exp(self.recon_log_var).view(
-                1, self.data_dims[0], 1, 1, 1)
-            log_var = self.recon_log_var
+        var = torch.exp(self.recon_log_var).view(1, D, 1, 1, 1)
+        log_var = self.recon_log_var
 
-        loss = 0.5 * (c * torch.log(2 * self.pi) + log_var.sum()) \
-            + 0.5 * torch.sum(1/var * diff, dim=1, keepdim=True)
-
-        if self.use_analytical_recon:
-            # set parameter for logging
-            self.recon_log_var = torch.nn.Parameter(
-                log_var.squeeze(), requires_grad=False)
+        loss = (D/2 * torch.log(2 * self.pi)  # factor n multiplication via boradcasting during addition
+                + 0.5 * log_var.sum()  # factor n multiplication via boradcasting during addition
+                + 0.5 * torch.sum(diff / var, dim=1, keepdim=True))
 
         if reduction == 'none':
             return loss
@@ -98,57 +89,54 @@ class ELBO(nn.Module):
             return t.mean()
 
         # we implement the term pixel-whise, summing over the channel dimension and mean over pixels if specified by the reduction
-        p = torch.prod(torch.tensor(self.data_dims[1:]))  # p pixels
-        n = self.ndims * p  # n displacement-vector components
-        degree = 2 * self.ndims  # degree matrix
+        n = torch.prod(torch.tensor(self.data_dims[1:]))  # n pixels
+        D = self.ndims  # D dimensions
+        neighborhood_size = 2 * self.ndims  # degree matrix
 
-        # log_det_p and translation terms are devided by factor p,
-        # as the scalar will be expanded (broadcasted) to size p during summation of the loss terms
-        # we mean aross the transformation channels for all terms
+        # log_det_p and translation terms are devided by factor n,
+        # as the scalar will be expanded (broadcasted) to size n during summation of the loss terms
+        # we sum aross the transformation channels for all terms
         var = torch.sum(torch.exp(log_var), dim=1, keepdim=True)
         diffusion_reg = self.grad_norm(
             mu) + self.grad_norm(mu.flip(dims=[2, 3, 4])).flip(dims=[2, 3, 4])
-        translation_component = 1 / p * \
-            torch.sum(mu, dim=[1, 2, 3, 4], keepdim=True)**2 / self.ndims
+        translation_component = 1 / n * \
+            torch.sum(torch.sum(mu, dim=[2, 3, 4],
+                                keepdim=True)**2, dim=1, keepdim=True)
         log_det_q = torch.sum(log_var, dim=1, keepdim=True)
 
-        if self.use_analytical_prior:
-            alpha = (n-1) / expect(degree * var + diffusion_reg)
-            beta = n**2 / expect(var + translation_component)
+        if self.training:
+            # use expectations over batch
+            alpha = (n-1) * D / expect(neighborhood_size * var + diffusion_reg)
+            beta = n**2 * D / expect(var + translation_component)
             log_alpha = torch.log(alpha)
             log_beta = torch.log(beta)
+            # add to running mean
+            self.running_mean_log_alpha(log_alpha.expand(1, 1, 2))
+            self.running_mean_log_beta(log_beta.expand(1, 1, 2))
         else:
-            alpha = torch.exp(self.prior_log_alpha)
-            beta = torch.exp(self.prior_log_beta)
-            log_alpha = self.prior_log_alpha
-            log_beta = self.prior_log_beta
+            # take alpha, beta from running means
+            alpha = torch.exp(self.log_alpha)
+            beta = torch.exp(self.log_beta)
+            log_alpha = self.log_alpha
+            log_beta = self.log_beta
 
-        log_det_p = 1/p * (
-            - (n-1) * torch.log(alpha)
-            - torch.log(beta)
+        log_det_p = 1/n * (
+            - (n-1) * D * log_alpha
+            - D * log_beta
         )
 
-        if self.use_analytical_prior:
-            # analytical solution for alpha, beta
-            loss = 0.5 * self.ndims * \
-                (log_det_p
-                 - log_det_q
-                 + (n-1) * (degree * var + diffusion_reg)
-                 / expect(degree * var + diffusion_reg)
-                 + (var + translation_component)
-                 / expect(var + translation_component)
-                 )
-            # set alpha, beta for logging
-            self.prior_log_alpha = torch.nn.Parameter(
-                torch.log(torch.as_tensor(alpha)), requires_grad=False)
-            self.prior_log_beta = torch.nn.Parameter(
-                torch.log(torch.as_tensor(beta)), requires_grad=False)
+        if self.training:
+            # calculate the shortened loss term, where some expectations cancel each other out
+            loss = 0.5 * (
+                log_det_p
+                - log_det_q
+            )
         else:
-            # parameterized by alpha, beta
-            loss = 0.5 * self.ndims * \
+            # calculate the whole loss term using alpha, beta
+            loss = 0.5 * \
                 (log_det_p
                  - log_det_q
-                 + (alpha * degree + beta / (n**2)) * var
+                 + (alpha * neighborhood_size + beta / (n**2)) * var
                  + alpha * diffusion_reg
                  + beta / (n**2) * translation_component
                  )
