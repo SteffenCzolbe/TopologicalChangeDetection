f5d60ea20adae238dd883e07451428c56dcf2b50
Steffen <per.sc@di.ku.dk>
2021-05-13 14:02:50 +0200

    300 epochs

--- scripts/train.sh
+++ scripts/train.sh
@@ -13,7 +13,7 @@ else
 fi
 
 # trainable recon, analytical prior
-$WRAPPER_FUNC python3 -m src.train_registration --dataset brain2d --channels 64 128 256 --recon_weight_init -5 --gpus -1 --accelerator dp --max_epochs 150 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --dropout --notest --fast_dev_run $FAST_DEV_RUN
+$WRAPPER_FUNC python3 -m src.train_registration --dataset brain2d --channels 64 128 256 --recon_weight_init -5 --gpus -1 --accelerator dp --max_epochs 300 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --dropout --notest --fast_dev_run $FAST_DEV_RUN
 mv lightning_logs/version_0 trained_models/mse/
 
 # train semantic loss model
@@ -21,5 +21,5 @@ $WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset brain2d --channels 64
 mv lightning_logs/version_0 trained_auxiliary_models/semantic_loss_feature_extractor/
 
 # semantic loss, trainable recon, analytical prior
-$WRAPPER_FUNC python3 -m src.train_registration --dataset brain2d --channels 64 128 256 --semantic_loss trained_auxiliary_models/semantic_loss_feature_extractor/ --recon_weight_init -11.5 --gpus -1 --accelerator dp --max_epochs 150 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --dropout --notest --fast_dev_run $FAST_DEV_RUN
+$WRAPPER_FUNC python3 -m src.train_registration --dataset brain2d --channels 64 128 256 --semantic_loss trained_auxiliary_models/semantic_loss_feature_extractor/ --recon_weight_init -11.5 --gpus -1 --accelerator dp --max_epochs 300 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --dropout --notest --fast_dev_run $FAST_DEV_RUN
 mv lightning_logs/version_0 trained_models/semantic_loss/
