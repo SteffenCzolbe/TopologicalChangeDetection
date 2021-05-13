db66be50c34d24104be1301960d8c2da9fe44812
Steffen <per.sc@di.ku.dk>
2021-05-13 14:24:11 +0200

    adapted thresholds

--- src/eval/config.py
+++ src/eval/config.py
@@ -4,12 +4,12 @@ MODELS = {
             "model_cls": RegistrationModel,
             "display_name": "MSE",
             "color": "r",
-            "probability_range": (2, 20)},
+            "probability_range": (1.5, 10)},
     "semantic_loss": {"path": "trained_models/semantic_loss",
                       "model_cls": RegistrationModel,
                       "display_name": "Semantic Loss",
                       "color": "r",
-                      "probability_range": (-1300, -300)},
+                      "probability_range": (-1900, -500)},
 }
 
 FULL_MODELS = ["mse", "semantic_loss"]  # only full models of our method
