28434c62779fe3aea89638353ed69f5ea4de59a4
Steffen <per.sc@di.ku.dk>
2021-09-15 12:32:11 +0200

    more epochs

---
+++ scripts/train_seg_models.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# trains the models.
+FAST_DEV_RUN=0
+
+# Check if slurm compute cluster available. Submit as slurm job if possible.
+if sbatch -h &> /dev/null; then
+    echo "Submitting to slurm..."
+    WRAPPER_FUNC=scripts/slurm/slurm_submit.sh
+else
+    echo "Running locally..."
+    WRAPPER_FUNC=
+fi
+
+
+# bnorm, dropout
+$WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset brain2d --channels 32 64 64 --gpus -1 --accelerator dp --max_epochs 5000 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --dropout --notest
+$WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset platelet-em --channels 32 64 64 --gpus -1 --accelerator dp --max_epochs 5000 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --dropout --notest
+
+
+# weight decay, bnorm
+$WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset brain2d --channels 32 64 64 --gpus -1 --accelerator dp --max_epochs 5000 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --weight_decay 0.03 --notest
+$WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset platelet-em --channels 32 64 64 --gpus -1 --accelerator dp --max_epochs 5000 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --weight_decay 0.03 --notest
