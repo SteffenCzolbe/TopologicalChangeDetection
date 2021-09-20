20cda5e6bfe7f8937a035c018c04bd311125e043
Steffen <per.sc@di.ku.dk>
2021-09-20 11:35:26 +0200

    more tuning

--- scripts/train_seg_models.sh
+++ scripts/train_seg_models.sh
@@ -14,13 +14,17 @@ fi
 
 
 # test weight decays for brains
-$WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset brain2d --channels 32 64 64 --gpus -1 --accelerator dp --max_epochs 5000 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 16 --bnorm --weight_decay 0.01 --notest
-$WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset brain2d --channels 32 64 64 --gpus -1 --accelerator dp --max_epochs 5000 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 16 --bnorm --weight_decay 0.05 --notest
-$WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset brain2d --channels 32 64 64 --gpus -1 --accelerator dp --max_epochs 5000 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 16 --bnorm --weight_decay 0.1 --notest
-$WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset brain2d --channels 32 64 64 --gpus -1 --accelerator dp --max_epochs 5000 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 16 --bnorm --weight_decay 0.5 --notest
-$WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset brain2d --channels 32 64 64 --gpus -1 --accelerator dp --max_epochs 5000 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 16 --bnorm --weight_decay 1 --notest
-
+$WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset brain2d --channels 32 64 64 --gpus -1 --accelerator dp --max_steps 20000 --lr_decline_patience 5 --early_stop_patience 10 --batch_size 16 --bnorm --weight_decay 0.01 --notest
+$WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset brain2d --channels 32 64 64 --gpus -1 --accelerator dp --max_steps 20000 --lr_decline_patience 5 --early_stop_patience 10 --batch_size 16 --bnorm --weight_decay 0.005 --notest
+$WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset brain2d --channels 32 64 64 --gpus -1 --accelerator dp --max_steps 20000 --lr_decline_patience 5 --early_stop_patience 10 --batch_size 16 --bnorm --weight_decay 0.001 --notest
+$WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset brain2d --channels 32 64 64 --gpus -1 --accelerator dp --max_steps 20000 --lr_decline_patience 5 --early_stop_patience 10 --batch_size 16 --bnorm --weight_decay 0.0005 --notest
+# mv lightning_logs/version_0 weights/brain2d/semantic_loss
 
 
 # platelet with weight decay 0.03
-# $WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset platelet-em --channels 32 64 64 --gpus -1 --accelerator dp --max_epochs 5000 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 16 --bnorm --weight_decay 0.03 --notest
+# $WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset platelet-em --channels 32 64 64 --gpus -1 --accelerator dp --max_steps 2000 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 16 --bnorm --weight_decay 0.03 --notest
+# mv lightning_logs/version_0 weights/platelet-em/semantic_loss
+
+# brain with weight decay of 0.01
+# $WRAPPER_FUNC python3 -m src.train_semantic_loss --dataset brain2d --channels 32 64 64 --gpus -1 --accelerator dp --max_steps 20000 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 16 --bnorm --weight_decay 0.01 --notest
+# mv lightning_logs/version_0 weights/brain2d/semantic_loss
