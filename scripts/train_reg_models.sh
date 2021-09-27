045b810efaaf66e0cbbe58bb51360029e89d45a5
Steffen <per.sc@di.ku.dk>
2021-09-27 16:12:47 +0200

    adjusted save paths

--- scripts/train_reg_models.sh
+++ scripts/train_reg_models.sh
@@ -16,9 +16,10 @@ fi
 $WRAPPER_FUNC python3 -m src.train_registration --dataset brain2d --channels 64 128 256 --semantic_loss weights/brain2d/semantic_loss/weight_decay0.0005 --recon_weight_init -11.5 --gpus -1 --accelerator dp --max_epochs 2500 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --weight_decay 0.0005 --notest --fast_dev_run $FAST_DEV_RUN
 $WRAPPER_FUNC python3 -m src.train_registration --dataset brain2d --channels 64 128 256 --semantic_loss weights/brain2d/semantic_loss/weight_decay0.0005 --recon_weight_init -11.5 --gpus -1 --accelerator dp --max_epochs 2500 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --weight_decay 0.01 --notest --fast_dev_run $FAST_DEV_RUN
 $WRAPPER_FUNC python3 -m src.train_registration --dataset brain2d --channels 64 128 256 --semantic_loss weights/brain2d/semantic_loss/weight_decay0.0005 --recon_weight_init -11.5 --gpus -1 --accelerator dp --max_epochs 2500 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --weight_decay 0.0001 --notest --fast_dev_run $FAST_DEV_RUN
+#mv lightning_logs/version_0 weights/brain2d/topology_detection/semantic_loss/weight_decay0.01
 
 #$WRAPPER_FUNC python3 -m src.train_registration --dataset platelet-em --channels 64 128 256 --semantic_loss weights/platelet-em/semantic_loss/weight_decay0.3 --recon_weight_init -11.5 --gpus -1 --accelerator dp --max_epochs 500 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --weight_decay 0.01 --notest --fast_dev_run $FAST_DEV_RUN
-#mv lightning_logs/version_0 weights/platelet-em/topology_detection/weight_decay0.01
+#mv lightning_logs/version_0 weights/platelet-em/topology_detection/semantic_loss/weight_decay0.01
 
 # l2 model
 #$WRAPPER_FUNC python3 -m src.train_registration --dataset brain2d --channels 64 128 256 --recon_weight_init -5 --gpus -1 --accelerator dp --max_epochs 250 --lr_decline_patience 50 --early_stop_patience 80 --batch_size 32 --bnorm --dropout --notest --fast_dev_run $FAST_DEV_RUN
